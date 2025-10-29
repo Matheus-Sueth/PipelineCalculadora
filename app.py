896329522b3e65195afe1d7312e691f35c9dd5b3
Matheus Almeida S Mendonça <matheuzengenharia@gmail.com>
2025-10-29 19:46:29 -0300

    feat: adicionando a funconalidade de mmultiplicação

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ while True:
     primeiro_numero = int(input("Digite o primeiro número:"))
     segundo_numero = int(input("Digite o segundo número:"))
 
-    opcao = input("\n\nQual opção você deseja:\n1 - Somar\n2 - Subtrair\n0 - Sair\nOpção: ")
+    opcao = input("\n\nQual opção você deseja:\n1 - Somar\n2 - Subtrair\n3 - Multiplicar\n0 - Sair\nOpção: ")
 
     if opcao == "1":
         resultado = primeiro_numero + segundo_numero
         print(f"Resultado: {resultado}")
     elif opcao == "2":
         resultado = primeiro_numero - segundo_numero
+        print(f"Resultado: {resultado}")
+    elif opcao == "3":
+        resultado = primeiro_numero * segundo_numero
         print(f"Resultado: {resultado}")
     elif opcao == "0":
         print("\nAté logo")
