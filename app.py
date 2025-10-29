c918687edb5e7bd4c467b2812d99958ff5b5d81b
Matheus Almeida S Mendonça <matheuzengenharia@gmail.com>
2025-10-29 19:43:59 -0300

    feat: adicionando a funconalidade de soma e subtração

---
+++ app.py
@@ -0,0 +1,19 @@
+print("BEM VINDO AO PROJETO CALCULADORA\n")
+while True:
+    primeiro_numero = int(input("Digite o primeiro número:"))
+    segundo_numero = int(input("Digite o segundo número:"))
+
+    opcao = input("\n\nQual opção você deseja:\n1 - Somar\n2 - Subtrair\n0 - Sair\nOpção: ")
+
+    if opcao == "1":
+        resultado = primeiro_numero + segundo_numero
+        print(f"Resultado: {resultado}")
+    elif opcao == "2":
+        resultado = primeiro_numero - segundo_numero
+        print(f"Resultado: {resultado}")
+    elif opcao == "0":
+        print("\nAté logo")
+        break
+    else:
+        print("\nOpção inválida, tente novamente")
+    print("\n")
